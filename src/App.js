753f0eb9bf0508154fa00f70c6f6fa9af4c0c3a1
Mathavan2003 <108514155+Mathavan2003@users.noreply.github.com>
2023-04-05 11:02:08 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Let we see the output of JavaScript
+        </h1>
+        <button onClick={window['alertMsg']}>Click to call msgAlert</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
